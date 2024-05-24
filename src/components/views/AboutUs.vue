7ecac7d3387b633b5e2e8976602e33e0eb4685cf
brandon vergara <brandon.vergara@nuatechnology.com>
2024-05-24 14:52:16 -0500

    Agregar clases CSS para estilizar el texto

--- src/components/views/AboutUs.vue
+++ src/components/views/AboutUs.vue
@@ -1,25 +1,31 @@
 <template>
   <div class="aboutus relative min-h-screen">
-    <Navbar class="navbar"/>
+
+    <div class="mt-6">
+      <Navbar class="navbar" />
+    </div>
+
 
     <!-- Contenedor para el título -->
-    <div class="container mx-auto px-4 mt-8">
+    <div class="container mx-auto px-4 mt-8 ">
       <div class="max-w-xl md:max-w-2xl lg:max-w-3xl"> <!-- Establecer un ancho máximo -->
         <h2 class="text-xl md:text-2xl lg:text-3xl font-bold text-black mb-3">Nosotros</h2>
         <h1 class="text-3xl md:text-5xl lg:text-6xl font-bold text-black mb-8">
-          <span class="text-4xl md:text-5xl lg:text-6xl inline-block font-bold md:text-left">Construyendo el futuro financiero de su empresa, <span class="text-blue-500">juntos</span></span>
+          <span class="text-4xl md:text-5xl lg:text-6xl inline-block font-bold md:text-left">Construyendo el futuro
+            financiero de su </span>
+          <span>empresa, <span class="text-blue-500">juntos</span> </span>
         </h1>
       </div>
     </div>
 
     <!-- Imagen de lado a lado -->
     <img src="../../assets/AboutUs/Frame 14228.png" alt="Imagen" class="w-full" />
-    <Newspaper/>
-    <Carousel/>
-    <Pillars/>
-    <Ready/> <!-- Listo para alcanzar las metas -->
-    <Whatsapp/><!-- Boton de WA -->
-    
+    <Newspaper />
+    <Carousel />
+    <Pillars />
+    <Ready /> <!-- Listo para alcanzar las metas -->
+    <Whatsapp /><!-- Boton de WA -->
+
   </div>
   <Footer />
 </template>
@@ -49,11 +55,12 @@ export default {
 
 <style scoped>
 .aboutus {
-    margin: 20px auto; 
-    max-width: 1400px; 
-    
+  /*  margin: 0; */
+  margin: 0 auto;
+  max-width: 1400px;
 }
+
 .w-full {
-  width: 100% !important; 
+  width: 100% !important;
 }
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,28 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Work Sans', 'sans-serif'],
+        'sans-medium': ['Work Sans Medium', 'sans-serif'],
+        'sans-semibold': ['Work Sans Semibold', 'sans-serif'],
+        'urbanist-black': ['Urbanist black', 'sans-serif'],
+      },
+      fontSize: {
+        '17': '17px',
+        '20': '20px',
+        '16': '16px',
+        '70': '70px',
+      },
+      letterSpacing: {
+        'tightest': '-0.05em',
+        'tighter': '-0.03em',
+        'normal': '0',
+      },
+      lineHeight: {
+        '98': '98.1%',
+      },
+    },
   },
   plugins: [],
 }
